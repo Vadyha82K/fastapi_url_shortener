88488d7452c26f07cfdbffee287a957cf60098c2
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-04-28 20:20:04 +0300

    Создал PUT, обновление записей

--- url-shortener/schemas/short_url.py
+++ url-shortener/schemas/short_url.py
@@ -1,12 +1,15 @@
 from typing import Annotated
 
-from annotated_types import Len
+from annotated_types import MaxLen, MinLen
 from pydantic import BaseModel, AnyHttpUrl
 
 
 class ShortUrlBase(BaseModel):
     target_url: AnyHttpUrl
-    slug: str
+    description: Annotated[
+        str,
+        MaxLen(200),
+    ] = ""
 
 
 class ShortUrlCreate(ShortUrlBase):
@@ -16,7 +19,19 @@ class ShortUrlCreate(ShortUrlBase):
 
     slug: Annotated[
         str,
-        Len(min_length=3, max_length=10),
+        MinLen(3),
+        MaxLen(10),
+    ]
+
+
+class ShortUrlUpdate(ShortUrlBase):
+    """
+    Модель обновления информации о сокращенной ссылке
+    """
+
+    description: Annotated[
+        str,
+        MaxLen(200),
     ]
 
 
@@ -24,3 +39,5 @@ class ShortUrl(ShortUrlBase):
     """
     Модель сокращенной ссылки
     """
+
+    slug: str
