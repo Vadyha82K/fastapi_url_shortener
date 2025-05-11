5170f33bbed5f398c0c1ad223eb95731fc40480d
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-05-11 23:17:30 +0300

    Перенес зависимости backround_task из функций в зависимости роутера

--- url-shortener/api/api_v1/short_urls/dependencies.py
+++ url-shortener/api/api_v1/short_urls/dependencies.py
@@ -1,8 +1,15 @@
-from fastapi import HTTPException
+import logging
+from fastapi import (
+    HTTPException,
+    BackgroundTasks,
+)
 from starlette import status
 
 from .crud import storage
 from schemas.short_url import ShortUrl
+
+
+log = logging.getLogger(__name__)
 
 
 def prefetch_short_url(slug: str) -> ShortUrl:
@@ -14,3 +21,13 @@ def prefetch_short_url(slug: str) -> ShortUrl:
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"URL {slug!r} not found",
     )
+
+
+def save_storage_state(
+    background_tasks: BackgroundTasks,
+):
+    # Сначала код до входа внутрь view функции
+    yield
+    # Код после покидания view функции
+    log.info("Add background task to save storage")
+    background_tasks.add_task(storage.save_state)
