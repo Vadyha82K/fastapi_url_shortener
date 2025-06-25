34173f1b7a5804f759add1fe77388c4b8822e9d0
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-06-25 19:53:44 +0300

    Параметризировал фикстуры

--- url-shortener/testing/conftest.py
+++ url-shortener/testing/conftest.py
@@ -16,19 +16,34 @@ if getenv("TESTING") != "1":
     raise pytest.exit(error_message)
 
 
-def create_short_url() -> ShortUrl:
-    short_url_in = ShortUrlCreate(
-        slug="".join(
-            random.choices(string.ascii_letters, k=8),  # noqa: S311
-        ),
+def build_short_url_create(slug: str) -> ShortUrlCreate:
+    return ShortUrlCreate(
+        slug=slug,
         description="A short url",
         target_url="https://example.com",
     )
+
+
+def build_short_url_create_random_slug() -> ShortUrlCreate:
+    return build_short_url_create(
+        slug="".join(
+            random.choices(string.ascii_letters, k=8),  # noqa: S311
+        ),
+    )
+
+
+def create_short_url(slug: str) -> ShortUrl:
+    short_url_in = build_short_url_create(slug=slug)
+    return storage.create(short_url_in)
+
+
+def create_short_url_random_slug() -> ShortUrl:
+    short_url_in = build_short_url_create_random_slug()
     return storage.create(short_url_in)
 
 
 @pytest.fixture()
 def short_url() -> Generator[ShortUrl]:
-    short_url = create_short_url()
+    short_url = create_short_url_random_slug()
     yield short_url
     storage.delete(short_url)
